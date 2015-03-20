dba2d21b3b6f651ec1731ed11da1957576d95887
Daniel Roberts <danielrobertsdesign@gmail.com>
2015-03-20 15:29:38 -0400

    Remove duplication

--- gilded_rose.rb
+++ gilded_rose.rb
@@ -42,11 +42,7 @@ def update_item(item)
   if item.sell_in < 0
     if item.name != "Aged Brie"
       if item.name != 'Backstage passes to a TAFKAL80ETC concert'
-        if item.quality > 0
-          if item.name != 'Sulfuras, Hand of Ragnaros'
-            item.quality -= 1
-          end
-        end
+        decrease_quality(item)
       else
         item.quality = item.quality - item.quality
       end

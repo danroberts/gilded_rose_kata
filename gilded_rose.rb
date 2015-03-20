a364db7a2a26fa676f7823f7f52e5b02e4aca24f
Daniel Roberts <danielrobertsdesign@gmail.com>
2015-03-20 15:24:09 -0400

    Extract method => decrease_quality

--- gilded_rose.rb
+++ gilded_rose.rb
@@ -4,13 +4,17 @@ def update_sell_in(item)
   end
 end
 
+def decrease_quality(item)
+  if item.quality > 0
+    if item.name != 'Sulfuras, Hand of Ragnaros'
+      item.quality -= 1
+    end
+  end
+end
+
 def update_item(item)
   if item.name != 'Aged Brie' && item.name != 'Backstage passes to a TAFKAL80ETC concert'
-    if item.quality > 0
-      if item.name != 'Sulfuras, Hand of Ragnaros'
-        item.quality -= 1
-      end
-    end
+    decrease_quality(item)
   else
     if item.quality < 50
       item.quality += 1
